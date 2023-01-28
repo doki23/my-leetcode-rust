90cbeed2118230d9010bd2a4342fe0e2c2bc70ba
doki <tunyum94@gmail.com>
2023-01-28 22:56:18 +0800

    2211

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod s_13;
 mod s_1848;
 mod s_1889;
+mod s_2211;
 mod s_433;
 mod s_447;
 mod s_835;
---
+++ src/s_2211.rs
@@ -0,0 +1,98 @@
+use crate::Solution;
+use std::mem;
+
+impl Solution {
+    pub fn count_collisions(directions: String) -> i32 {
+        let mut chars = directions.chars().collect::<Vec<char>>();
+        let mut count = 0;
+        let mut i = 0;
+        let len = directions.len();
+        while i < len {
+            let curr_dir = chars[i];
+            let right_dir = if i < len - 1 {
+                Some(chars[i + 1])
+            } else {
+                None
+            };
+            match (curr_dir, right_dir) {
+                ('R', Some('R'))
+                | ('L', Some('R'))
+                | ('L', Some('L'))
+                | ('L', Some('S'))
+                | ('S', Some('R'))
+                | ('S', Some('S'))
+                | (_, None) => i += 1,
+                ('R', Some('L')) => {
+                    count += 2;
+                    let _ = mem::replace(&mut chars[i], 'S');
+                    let _ = mem::replace(&mut chars[i + 1], 'S');
+                    if i >= 1 {
+                        i -= 1;
+                    } else {
+                        i += 1;
+                    }
+                }
+                ('R', Some('S')) => {
+                    count += 1;
+                    let _ = mem::replace(&mut chars[i], 'S');
+                    if i >= 1 {
+                        i -= 1;
+                    } else {
+                        i += 1;
+                    }
+                }
+                ('S', Some('L')) => {
+                    count += 1;
+                    let _ = mem::replace(&mut chars[i + 1], 'S');
+                    i += 1;
+                }
+                _ => unreachable!(),
+            }
+        }
+        count
+    }
+}
+
+#[test]
+fn test_cases() {
+    struct TestCase {
+        input: String,
+        answer: i32,
+    }
+
+    let mut test_cases = Vec::new();
+
+    let t1 = TestCase {
+        input: "RLRSLL".into(),
+        answer: 5,
+    };
+    test_cases.push(t1);
+
+    let t2 = TestCase {
+        input: "LLRR".into(),
+        answer: 0,
+    };
+    test_cases.push(t2);
+
+    let t3 = TestCase {
+        input: "L".into(),
+        answer: 0,
+    };
+    test_cases.push(t3);
+
+    let t4 = TestCase {
+        input: "R".into(),
+        answer: 0,
+    };
+    test_cases.push(t4);
+
+    let t5 = TestCase {
+        input: "RRRLLL".into(),
+        answer: 6,
+    };
+    test_cases.push(t5);
+
+    for tc in test_cases {
+        assert_eq!(tc.answer, Solution::count_collisions(tc.input));
+    }
+}
